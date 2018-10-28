f62fa631094957dd375af7396ff7787c35776787
Erik <erikzb7@gmail.com>
2018-10-27 20:36:40 -0400

    Few changes to server.js and package.json

--- database/index.js
+++ database/index.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
-
+const config =
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+      }
+    : {
+        host: "127.0.0.1",
+        user: process.env.USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: "blog-site"
+      };
 const knex = require("knex")({
   client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: process.env.USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: "blog-site"
-  }
+  connection: config
 });
 module.exports = knex;
--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const routes = require("./routes/routes");
 const cors = require("cors");
+const PATH = require("path");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/", routes);
 
+// Server Static Assets In Production
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static(__dirname + "/client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(PATH.resolve(__dirname + "/client/build", "index.html"));
+  });
+}
 // Start Server
 app.listen(PORT, () => {
   console.log(`Listening in ${PORT}`);
