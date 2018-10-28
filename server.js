4e1e28a86b4b9b8038b4a7f273ee60e6b41c411e
Erik <erikzb7@gmail.com>
2018-10-27 21:01:41 -0400

    Server js changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
   // Set Static Folder
   app.use(express.static(__dirname + "/client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(PATH.resolve(__dirname + "/client/build", "index.html"));
+    res.sendFile(PATH.resolve(__dirname + "client", "build", "index.html"));
   });
 }
 // Start Server
